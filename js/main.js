9f0510651cf37400518caccdac62ff5951fe80da
Neelima Chinta <neelimachinta04@gmail.com>
2021-01-06 18:24:30 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+
+    $nav = $('.nav');
+    $toggleCollapse = $('.toggle-collapse');
+
+    /** click event on toggle menu */
+    $toggleCollapse.click(function () {
+        $nav.toggleClass('collapse');
+    })
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        dots: false,
+        nav: true
+    });
+
+});
